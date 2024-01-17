e90b7914d166800ebffd48217c5ab73135614dcc
Imani Niyigena <ImaniN@hollywoodbets.net>
2024-01-17 10:40:24 +0200

    Favicon addition + custom colour addition + VS Code WorkSpace addition

--- deposits-ui-demo/tailwind.config.js
+++ deposits-ui-demo/tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        rebeccapurple: "#633D94",
+        aureolin: "#FBED21",
+        jet: "#373435",
+      },
+    },
   },
   plugins: [],
 };
